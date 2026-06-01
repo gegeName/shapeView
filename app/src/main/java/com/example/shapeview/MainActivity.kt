dcb677a48b9812523ebef8515ad3f4dc8ad965e8
gegeName <20966610+gegeName@users.noreply.github.com>
2026-06-01 22:04:34 +0800

    ...

--- app/src/main/java/com/example/shapeview/MainActivity.kt
+++ app/src/main/java/com/example/shapeview/MainActivity.kt
@@ -16,5 +16,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        findViewById<android.view.View>(R.id.btn_color).setOnClickListener { view ->
+            view.isSelected = !view.isSelected
+        }
     }
 }
